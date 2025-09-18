6ee98132802755ed8359201dba1ad3579e46af23
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-17 20:18:24 -0400

    updated Main with some test code

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         SimpleRemoteControl remote = new SimpleRemoteControl();
+        Light light = new Light();
+        LightOnCommand lightOn = new LightOnCommand(light);
+
+        remote .setCommand(lightOn);
+        remote.buttonWasPressed();
 
 
         }
