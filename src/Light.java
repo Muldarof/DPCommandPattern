200296f7ee37c8b0610a23713aac4d3a8728ab69
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-17 20:01:48 -0400

    added light and light on command

---
+++ src/Light.java
@@ -0,0 +1,19 @@
+public class Light {
+    boolean status;
+
+    public Light(){
+        status = false;
+    }
+
+    public void on(){
+        status = true;
+        System.out.println("Light is On");
+    }
+
+    public void off(){
+        status = false;
+        System.out.println("Light is Off");
+    }
+
+
+}
