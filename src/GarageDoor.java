ae5717830b4f38cb0e529b610fd86f0f651a2624
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-17 20:27:13 -0400

    created Garage Door object

---
+++ src/GarageDoor.java
@@ -0,0 +1,26 @@
+public class GarageDoor {
+
+    public GarageDoor(){
+
+    }
+
+    public void up(){
+        System.out.println("Garage Door Up");
+    }
+
+    public void down(){
+        System.out.println("Garage Door Down");
+    }
+
+    public void stop(){
+        System.out.println("Garage Door Stopped");
+    }
+
+    public void lightOn(){
+        System.out.println("Garage Light On");
+    }
+
+    public void lightOff(){
+        System.out.println("Garage Light Off");
+    }
+}
