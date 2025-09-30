6beff9d96df1d7a7d640550a3f9b915741f6d95f
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-29 22:36:58 -0400

    added Ceiling Fan Object and FanSpeed Enum

---
+++ src/CeilingFan.java
@@ -0,0 +1,29 @@
+public class CeilingFan {
+    FanSpeed fanSpeed;
+
+    public CeilingFan() {
+        fanSpeed = FanSpeed.OFF;
+    }
+
+    public void high(){
+        System.out.println("Ceiling Fan On High");
+        fanSpeed = FanSpeed.HIGH;
+    }
+    public void medium(){
+        System.out.println("Ceiling Fan On Medium");
+        fanSpeed = FanSpeed.MEDIUM;
+    }
+    public void low(){
+        System.out.println("Ceiling Fan On Low");
+        fanSpeed = FanSpeed.LOW;
+    }
+    public void off(){
+        System.out.println("Ceiling Fan Off");
+        fanSpeed = FanSpeed.OFF;
+    }
+    public String getSpeed(){
+        return fanSpeed.toString();
+    }
+
+
+}
---
+++ src/FanSpeed.java
@@ -0,0 +1,6 @@
+public enum FanSpeed {
+    HIGH,
+    MEDIUM,
+    LOW,
+    OFF
+}
