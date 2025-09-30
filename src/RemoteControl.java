6b33a33086e30d34768675e3ed2af6d7c586926f
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-29 22:04:05 -0400

    added RemoteControl

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -8,9 +8,34 @@ public class RemoteControl {
         offCommands = new Command[7];
 
         Command noCommand = new noCommand();
-
+        for(int i = 0; i < 7; i++){
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
 
     }
 
+    public void setCommand(int slot, Command onCommand, Command offCommand){
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot){
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot){
+        offCommands[slot].execute();
+    }
+
+    public String toString(){
+        StringBuffer stringBuff = new StringBuffer();
+        stringBuff.append("\n----- Remote Control -----\n");
+        for(int i = 0; i < onCommands.length; i++){
+            stringBuff.append("[slot " + i + "] " + onCommands[i].getClass().getName()
+            + "     " + offCommands[i].getClass().getName() + "\n");
+        }
+        return stringBuff.toString();
+    }
 
 }
