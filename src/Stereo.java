db3ca6ffe0813d6e478efb8448bfe78791338a1f
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-29 22:09:58 -0400

    added Stereo object

---
+++ src/Stereo.java
@@ -0,0 +1,25 @@
+public class Stereo {
+    public Stereo() {
+    }
+
+    public void on(){
+        System.out.println("Stereo On");
+    }
+    public void off(){
+        System.out.println("Stereo Off");
+    }
+    public void setCd(){
+        System.out.println("CD Player Active");
+    }
+    public void setDvd(){
+        System.out.println("DVD Player Active");
+    }
+    public void setRadio(){
+        System.out.println("Radio Active");
+    }
+    public void setVolume(){
+        System.out.println("Set Volume to 11");
+    }
+
+
+}
