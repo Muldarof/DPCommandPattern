329caa13f776e445231c425dce5d4aa6e775ffd7
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-09-29 22:42:35 -0400

    added Television Object

---
+++ src/Television.java
@@ -0,0 +1,18 @@
+public class Television {
+
+    public Television() {
+    }
+
+    public void on(){
+        System.out.println("TV On");
+    }
+    public void off(){
+        System.out.println("TV Off");
+    }
+    public void setInputChannel(int channel){
+        System.out.println("Set Channel to " + channel);
+    }
+    public void setVolume(int volume){
+        System.out.println("Set Volume to " + volume);
+    }
+}
