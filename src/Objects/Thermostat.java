a69c1dfa718b1382ab2a287d25290516c793d785
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-01 19:33:15 -0400

    added Object Thermostat

---
+++ src/Objects/Thermostat.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class Thermostat {
+    float temp;
+
+    public Thermostat() {
+        temp = 0;
+    }
+
+    public void setTemp(float temp) {
+        this.temp = temp;
+    }
+}
