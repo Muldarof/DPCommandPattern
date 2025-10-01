4d7525d0902dfbf31becbbe8fa030e0206e2957b
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-01 19:08:51 -0400

    added Object Garden Light

---
+++ src/GardenLight.java
@@ -0,0 +1,27 @@
+import java.time.LocalTime;
+
+public class GardenLight {
+    LocalTime duskTime;
+    LocalTime dawnTime;
+
+    public GardenLight() {
+        duskTime = LocalTime.of(20,0,0);
+        dawnTime = LocalTime.of(6,0,0);
+    }
+
+    public void setDuskTime(LocalTime time){
+        duskTime = time;
+        System.out.println("Setting Dusktime to: " + time);
+    }
+    public void setDawnTime(LocalTime time){
+        dawnTime = time;
+        System.out.println("Setting DawnTime to: " + time);
+    }
+    public void manualOn(){
+        System.out.println("Garden Light On");
+    }
+    public void manualOff(){
+        System.out.println("Garden Light Off");
+    }
+
+}
