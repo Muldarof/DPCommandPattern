406d7f37f0a9e2920a0bfcee037989e68f0c6090
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-01 19:31:29 -0400

    Added Object Faucet

---
+++ src/Objects/Faucet.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class Faucet {
+
+    public Faucet() {
+    }
+    public void openValve(){
+        System.out.println("Faucet Valve is Open");
+    }
+    public void closeValve(){
+        System.out.println("Faucet Valve is Closed");
+    }
+}
