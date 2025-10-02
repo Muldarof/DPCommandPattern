03977e2c96a712cc74a326c232e2b69bbd3d02a9
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-01 20:05:35 -0400

    added Object Security Control

---
+++ src/Objects/SecurityControl.java
@@ -0,0 +1,12 @@
+package Objects;
+
+public class SecurityControl {
+    public SecurityControl() {
+    }
+    public void arm(){
+        System.out.println("Security System Armed");
+    }
+    public void disarm(){
+        System.out.println("Security System Disarmed");
+    }
+}
