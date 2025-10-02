fbea3392c7546c4c1b466b797932de3286a0d2ae
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-01 20:07:18 -0400

    added Object Water Sprinkler

---
+++ src/Objects/Sprinkler.java
@@ -0,0 +1,12 @@
+package Objects;
+
+public class Sprinkler {
+    public Sprinkler() {
+    }
+    public void waterOn(){
+        System.out.println("Water Sprinkler On");
+    }
+    public void waterOff(){
+        System.out.println("Water Sprinkler Off");
+    }
+}
