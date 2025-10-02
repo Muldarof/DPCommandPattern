b96b1452d6da618bd35df7f2ab8e8db750ae3cb7
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-01 21:30:31 -0400

    added Command for hottub on and off

---
+++ src/Commands/HottubOffCommand.java
@@ -0,0 +1,15 @@
+package Commands;
+
+import Objects.Hottub;
+
+public class HottubOffCommand implements Command{
+    Hottub hottub;
+
+    public HottubOffCommand(Hottub hottub) {
+        this.hottub = hottub;
+    }
+
+    public void execute() {
+        hottub.jetsOff();
+    }
+}
---
+++ src/Commands/HottubOnCommand.java
@@ -0,0 +1,17 @@
+package Commands;
+
+import Objects.Hottub;
+
+public class HottubOnCommand implements Command{
+    Hottub hottub;
+
+    public HottubOnCommand(Hottub hottub) {
+        this.hottub = hottub;
+    }
+
+    public void execute() {
+        hottub.jetsOn();
+        hottub.circulate();
+        hottub.setTemperature(80);
+    }
+}
