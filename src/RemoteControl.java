329f3febbdced1bc6e526f276df1da2774ceef5f
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-10-03 21:28:10 -0400

    refactor updated Remote Control with undo functionality

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -5,6 +5,7 @@ public class RemoteControl {
 
     Command[] onCommands;
     Command[] offCommands;
+    Command undoCommand;
 
     public RemoteControl() {
         onCommands = new Command[7];
@@ -15,7 +16,7 @@ public class RemoteControl {
             onCommands[i] = noCommand;
             offCommands[i] = noCommand;
         }
-
+        undoCommand = noCommand;
     }
 
     public void setCommand(int slot, Command onCommand, Command offCommand){
@@ -25,10 +26,16 @@ public class RemoteControl {
 
     public void onButtonWasPushed(int slot){
         onCommands[slot].execute();
+        undoCommand = onCommands[slot];
     }
 
     public void offButtonWasPushed(int slot){
         offCommands[slot].execute();
+        undoCommand = offCommands[slot];
+    }
+
+    public void undoButtonWasPushed(){
+        undoCommand.undo();
     }
 
     public String toString(){
